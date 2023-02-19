c76b7b88728ab391afccc0ec8f84b29d2fd7a721
bemxio <bemxiov@protonmail.com>
2023-02-19 15:49:19 +0100

    add the echo bootloader to the sources

---
+++ src/echo.asm
@@ -0,0 +1,15 @@
+[bits 16] ; 16-bit code
+[org 0x7c00] ; bootloader offset
+
+loop:
+    mov ah, 0x00 ; read character
+    int 0x16 ; BIOS interrupt
+
+    mov ah, 0x0e ; print character
+    int 0x10 ; BIOS interrupt
+
+    jmp loop
+
+times 510 - ($ - $$) db 0 ; pad to 510 bytes
+
+dw 0xaa55 ; magic signature
