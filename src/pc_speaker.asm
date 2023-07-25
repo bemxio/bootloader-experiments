53e489c84436eac9b3d4b998d978e3ebe9cd5cb3
bemxio <bemxiov@protonmail.com>
2023-07-25 08:03:23 +0200

    add example code for playing a frequency

---
+++ src/pc_speaker.asm
@@ -0,0 +1,29 @@
+[bits 16] ; set the code to 16-bit mode
+[org 0x7c00] ; set the global offset (0x7c00 is where the BIOS loads the bootloader)
+
+; set the PIT to the desired frequency
+mov al, 0xb6 ; set the command byte
+out 0x43, al
+
+mov al, 0xa9 ; lower byte (frequency)
+out 0x42, al
+
+mov al, 0x04 ; upper byte (frequency)
+out 0x42, al
+
+; play the sound using the PC speaker
+in al, 0x61 ; read the port
+
+mov ah, al ; save the value
+or ah, 3 ; set bits 0 and 1
+
+cmp al, ah ; check if the value has changed
+je $ ; if not, jump to the current address
+
+mov al, ah ; save the new value
+out 0x61, al ; write the new value to the port
+
+; padding and magic number
+times 510 - ($ - $$) db 0 ; pad the rest of the sector with null bytes
+
+dw 0xaa55 ; set the magic number
