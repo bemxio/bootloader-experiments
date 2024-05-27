639d20fb32e0fa44227526cfda4bcc8725610493
bemxio <bemxiov@protonmail.com>
2024-05-27 21:39:09 +0200

    handle the backspace

--- src/user_interaction.asm
+++ src/user_interaction.asm
@@ -1,19 +1,26 @@
 [bits 16] ; set the code to 16-bit mode
 [org 0x7c00] ; set the global offset (0x7c00 is where the BIOS loads the bootloader)
 
-mov bp, INPUT_PROMPT ; set the base pointer to the input prompt address
+; user interaction stuff
+mov si, INPUT_PROMPT ; set the base pointer to the input prompt address
 call print ; call the print function
 
-mov bp, BUFFER ; set the base pointer to the buffer
+mov di, INPUT_BUFFER ; set the base pointer to the buffer
 call input ; call the input function
 
-mov bp, INPUT_RESPONSE ; set the base pointer to the input response address
+mov si, INPUT_RESPONSE ; set the base pointer to the input response address
 call print ; call the print function
 
-mov bp, BUFFER ; set the base pointer to the buffer
+mov si, INPUT_BUFFER ; set the base pointer to the buffer
 call print ; call the print function
 
-jmp $ ; infinite loop
+; a little exclamation mark :D
+mov ah, 0x0e ; set "Teletype Output" mode
+mov al, '!' ; move the exclamation mark to the register
+
+int 0x10 ; call the BIOS interrupt
+
+jmp $ ; loop forever
 
 ; functions
 print:
@@ -23,14 +30,14 @@ print:
     xor bx, bx ; set page number and foreground color to 0
 
     print_loop:
-        mov al, [bp] ; move the character from the base pointer to the register
+        mov al, [si] ; move the character from the base pointer to the register
 
         test al, al ; check if the character is null
         jz print_end ; if so, we're done
 
         int 0x10 ; call the BIOS interrupt
 
-        inc bp ; move to the next character
+        inc si ; move to the next character
         jmp print_loop ; repeat
     
     print_end:
@@ -47,11 +54,26 @@ input:
         cmp al, 0x0d ; check if the Enter key was pressed
         je input_end ; if so, we're done
 
+        cmp al, 0x08 ; check if the Backspace key was pressed
+        je input_backspace ; if so, handle backspace
+
         mov ah, 0x0e ; set "Teletype Output" mode
         int 0x10 ; call the BIOS interrupt
 
-        mov byte [bp], al ; move the character to the base pointer
-        inc bp ; move to the next character
+        mov byte [di], al ; move the character to the base pointer
+        inc di ; move to the next character
+
+        jmp input_loop ; repeat
+
+    input_backspace:
+        cmp di, INPUT_BUFFER ; check if we're at the beginning of the buffer
+        je input_loop ; if so, ignore the backspace
+
+        dec di ; move back one character
+        mov byte [di], 0x00 ; null-terminate the string
+
+        mov si, BACKSPACE_HANDLER ; set the base pointer to the backspace handler
+        call print ; call the print function
 
         jmp input_loop ; repeat
 
@@ -64,16 +86,18 @@ input:
         mov al, 0x0a ; move the newline character to the register
         int 0x10 ; call the BIOS interrupt
 
-        mov byte [bp], 0x00 ; null-terminate the string
+        mov byte [di], 0x00 ; null-terminate the string
 
         popa ; restore registers
         ret ; return from function
 
-; data
+; various data
 INPUT_PROMPT db "Enter your name: ", 0x00
 INPUT_RESPONSE db "Hello, ", 0x00
 
-BUFFER times 64 db 0x00
+INPUT_BUFFER times 64 db 0x00
+
+BACKSPACE_HANDLER db 0x08, 0x20, 0x08, 0x00
 
 ; pad the rest of the sector with null bytes
 times 510 - ($ - $$) db 0
